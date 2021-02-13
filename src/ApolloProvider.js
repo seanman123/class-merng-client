d40431f71e3417a6eaac6408c97495db168849f6
Sean Dunaway <contactsean123@gmail.com>
2021-02-13 11:50:22 -0500

    edited proxy and server base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -4,7 +4,7 @@ import {ApolloClient, ApolloProvider, InMemoryCache, createHttpLink} from '@apol
 import { setContext } from 'apollo-link-context'; 
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://limitless-citadel-47087.herokuapp.com/'
 })
 
 const authLink = setContext(() => {
